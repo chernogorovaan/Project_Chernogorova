1c68d6391380b73db09b57b8abf39b9377d768c9
chernogorovaan <anastasiacernogorova8@gmail.com>
2025-10-04 12:29:26 +0300

    Add files via upload

--- PZ_2_27.py
+++ PZ_2_27.py
@@ -1,7 +1,9 @@
-
-#Задача: определить сколько секунд прошло за последний час
-
-n = int(input("Введите количество прошедших секунд за день:"))
-seconds_hour = 3600
-seconds_last_hour = n % seconds_hour
-print(seconds_last_hour) #Выводим результат на консоль
+# Задача: определить сколько секунд прошло за последний час
+try:
+    n = int(input("Введите количество прошедших секунд за день:"))
+    seconds_hour = 3600
+    seconds_last_hour = n % seconds_hour
+    print(seconds_last_hour)
+    # Выводим результат на консоль
+except ValueError:
+    print("Ошибка! Введите целое число")
