044138797484b7e372e13328b68bf20082127d81
chernogorovaan <anastasiacernogorova8@gmail.com>
2025-11-15 12:11:45 +0300

    Update PZ_5_dop.py

--- PZ_5/PZ_5_dop.py
+++ PZ_5/PZ_5_dop.py
@@ -1,1 +1,25 @@
 #Задание: Даны три числа. Определить у какого числа больше сумма цифр. Вывод результата предусмотреть в основной программе. Расчет суммы цифр оформить в функции
+def NumSum(num):
+  total_sum = 0
+  for i in str(num):
+    total_sum+=int(i)
+  return total_sum
+ 
+def Numbers():
+  a=int(input(':'))
+  b=int(input(':'))
+  c=int(input(':'))
+  sum1=NumSum(a)
+  sum2=NumSum(b)
+  sum3=NumSum(c)
+  if sum1>sum2 and sum1>sum3:
+    print(sum1)
+  elif sum2>sum1 and sum2>sum3:
+    print(sum2)
+  elif sum3>sum1 and sum3>sum2:
+    print(sum3)
+  
+
+    
+    
+  
