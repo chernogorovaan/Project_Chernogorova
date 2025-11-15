58489bf92e0b1ee486c87a13db7e5652bacbf564
chernogorovaan <anastasiacernogorova8@gmail.com>
2025-11-15 14:08:49 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,5 +1,21 @@
+#Задание: Составить программу, в которой функция генерирует четырехзначное число и определяет есть ли в числе одинаковые цифры
+import random
 try:
   def NumbersGen():
-    
-  except ValueError:
+    num=random.randint(1000,9999)
+    d1=num//1000
+    d2=(num//100)%10
+    d3=(num//10)%10
+    d4=num%10
+    if d1==d2 or d1==d3 or d1==d4 or d2==d3 or d2==d4 or d3==d4:
+      print('True')
+      print(num)
+    else:
+      print('False')
+      print(num)
+
+except ValueError:
     print('Ошибка! Введите цифру')
+NumbersGen()
+
+
