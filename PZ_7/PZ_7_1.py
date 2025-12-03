257911c5428db565fece47e074030cee008d1d97
chernogorovaan <anastasiacernogorova8@gmail.com>
2025-12-03 12:13:32 +0300

    Update PZ_7_1.py

--- PZ_7/PZ_7_1.py
+++ PZ_7/PZ_7_1.py
@@ -1,11 +1,15 @@
 #Задание: Дан символ C и строка S. Удвоить каждое вхождение символа C в строку S
-c = input('Введите символ c')
-s = input('Введите стриоку s')
-new_s = ""
-for i in s:
-  if i==c:
-    new_s+=i*2
-  else:
-    new_s+=i
-  print(new_s)
-    
+try:
+    c = input('Введите символ c')
+    s = input('Введите стриоку s')
+    new_s = ""
+    for i in s:
+        if i==c:
+            new_s+=i*2
+        else:
+            new_s+=i
+    print(new_s)
+except ValueError:
+    print("Ошибка! Введите строку!")
+
+
