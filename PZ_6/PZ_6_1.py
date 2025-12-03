3949b2bc79a1b15d1c9354079ea3fa4784fa1c9e
chernogorovaan <anastasiacernogorova8@gmail.com>
2025-12-03 18:37:53 +0300

    Update PZ_6_1.py

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,10 +1,11 @@
 #Задание: Дан список A размера N. Найти минимальный элемент из его элементов с четными номерами A2, A4, A6...
+import random
 try:
-    n=int(input('введите количестов элементов списка:'))
+    n=random.randint(1,1000)
     a=list()
     min_el = None
     for i in range(n):
-        b=int(input(f"введите элемент {i}"))
+        b=random.randint(1,1000)
         a.append(b)
     for i in range(0,n,2):
         if min_el is None or a[i]<min_el:
