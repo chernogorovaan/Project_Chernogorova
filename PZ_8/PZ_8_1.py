24d53061a07001d3efdbc4d3b1b17baac697ab30
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-01-23 21:41:29 +0300

    commited

---
+++ PZ_8/PZ_8_1.py
@@ -0,0 +1,12 @@
+"""
+Удалите ключи ["name", "salary"] из sample_dict.
+"""
+sample_dict = {
+ "name": "Kelly",
+ "age": 25,
+ "salary": 8000,
+ "city": "New york"
+}
+del sample_dict ["name"]
+del sample_dict ["salary"]
+print(sample_dict)
