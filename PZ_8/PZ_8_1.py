fb9f2cf00ec64d810b6c01946e721e232093c3c3
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-01-24 17:41:55 +0300

    Update PZ_8_1.py

--- PZ_8/PZ_8_1.py
+++ PZ_8/PZ_8_1.py
@@ -2,13 +2,15 @@
 Удалите ключи ["name", "salary"] из sample_dict.
 """
 try:
-    my_dict = {
-    "небо": "sky", "земля": "earth", "море": "sea", "река": "river",
-    "гора": "mountain", "лес": "forest", "поле": "field", "цветок": "flower",
-    "дерево": "tree", "трава": "grass"
-    }
-
-    translator = input("Введите слово: ")
-    print(f"Перевод: {my_dict.get(translator, 'нет в словаре')}")
+  sample_dict = { 
+  "name": "Kelly", 
+  "age": 25, 
+  "salary": 8000, 
+  "city": "New york" 
+  }
+  del sample_dict["name"]
+  del sample_dict["salary"]
+  print(sample_dict)
 except AttributeError:
-    print("ошибка! у объекта нет такого атрибута или метода")
+
+  print("ошибка! у объекта нет такого атрибута или метода")
