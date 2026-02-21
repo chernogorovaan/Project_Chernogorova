2e4eb9b547d2a97edb1bfdf6efb7781f86588e19
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-02-21 11:48:29 +0300

    Update PZ_10_2.py

--- PZ_10/PZ_10_2.py
+++ PZ_10/PZ_10_2.py
@@ -1,1 +1,27 @@
+'''
+Из предложенного текстового файла (text18-27.txt) вывести на экран его содержимое,
+количество пробельных символов. Сформировать новый файл, в который поместить текст
+в стихотворной форме предварительно поставив последнюю строку фразой введенной
+пользователем.
+'''
+file = open('text18-27.txt', 'r')  
+text = file.read()                                    
+file.close() 
+print(text)
+
+spaces = 0
+for i in text:
+    if i == ' ' or i == '\n' or i == '\t':
+        spaces+=1
+print("Пробельных символов:", spaces)
+
+sayit = input("Введи фразу: ")
+strs = text.split('\n')
+strs[-1] = sayit
+new_text = '\n'.join(strs)
+
+f = open('new_file.txt', 'w')
+f.write(new_text)
+f.close()
+
 
