f9650592411a189431b83f4b8f1bc835825c59eb
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-03-13 15:32:44 +0300

    Update PZ_11_1.py

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -7,7 +7,6 @@ n = []
 for i in range (10):
     i=random.randint(-10,10)
     n.append(i)
-# n = [-1, 2, -3, 4, -5, -6, -7, -8]
 half = n[:len(n)//2]
 neg = [i for i in half if i<0]
 total = 1
@@ -15,3 +14,5 @@ for i in neg:
     total = total * i
 print(total)
 print(neg)
+print(n)
+print(half)
