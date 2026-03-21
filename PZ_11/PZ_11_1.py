96f77dbf19f2516d13ec3d70940aa9f5ff5ff88c
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-03-21 14:24:45 +0300

    Update PZ_11_1.py

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -3,11 +3,11 @@
 произведение элементов меньших 0.
 """
 import random
-import math
+from functools import reduce
 n = [random.randint(-10,10) for i in range(10)]
 half = n[:len(n)//2]
 neg = [i for i in half if i<0]
-total = math.prod(neg)
+total = reduce(lambda x,y: x*y, neg)
 print(total)
 print(neg)
 print(half)
