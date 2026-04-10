ea6e1a16c1df8b815d05c3cea6babbdc487bbaa1
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-04-10 15:22:00 +0300

    gfrgfv

---
+++ PZ_12/PZ_12_1.py
@@ -0,0 +1,15 @@
+'''
+1. В матрице найти среднее арифметическое положительных элементов, кратных 3.
+'''
+import random
+
+rows, cols = 3, 3
+matrix = [[random.randint(-10,10) for _ in range(cols)] for _ in range(rows)]
+print("Сгенерированная матрица 3x3:")
+print(*matrix, sep='\n')
+
+suitable_elements = [element for row in matrix for element in row if element > 0 and element % 3 == 0]
+
+result = sum(suitable_elements) / len(suitable_elements) if suitable_elements else None
+
+print("\nСреднее арифметическое положительных элементов, кратных 3:", result
