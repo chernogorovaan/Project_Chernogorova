d8ef22cb158592022aa5b852080da674e2756fb5
chernogorovaan <anastasiacernogorova8@gmail.com>
2026-04-29 20:56:37 +0300

    gghg

--- PZ_13/PZ_13.py
+++ PZ_13/PZ_13.py
@@ -3,17 +3,16 @@
 год и поместить ее в новый текстовый файл.
 '''
 import re
-with open('Dostoevsky.txt', 'r') as file:
+with open('Dostoevsky.txt', 'r' , encoding='utf-8') as file:
     text = file.read()
 
 pattern = r'(?:^|\n)(.*?1857.*?)(?=\n\d{4}|\Z)'
 
-# Или более простой вариант — найти абзац/блок, содержащий "1857"
+
 pattern2 = r'(?:^|\n)([^\n]*1857[^\n]*(?:\n[^\n\d]+)*)'
 
 match = re.search(pattern2, text, re.DOTALL | re.MULTILINE)
 
-# Запись результата в новый файл
 if match:
     with open('Dostoevsky_1857.txt', 'w', encoding='utf-8') as output_file:
         output_file.write(match.group(1))
